e162f58cdec6d2258c12983988ff20824b277052
Saigopini Panneerselvam <spanneerselvam@scu.edu>
2020-03-04 11:28:56 -0800

    Graph Implementation

---
+++ Data Structures/Ch. 4 Trees and Graphs/graphs.py
@@ -0,0 +1,33 @@
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+    def add_edge(self, node, neighbor=None):
+        edges = []
+        if neighbor != None:
+          if node not in self.graph:
+              edges.append(neighbor)
+              self.graph[node] = edges
+              self.add_edge(neighbor, node)
+          if node in self.graph:
+              edges = self.graph[node]
+              if neighbor not in edges:
+                  edges.append(neighbor)
+        else:
+          if node not in self.graph:
+              self.graph[node] = edges
+    def print_graph(self):
+        return self.graph
+    def show_vertices(self, mode):
+        keys = []
+        keys = self.graph.keys()
+        return keys
+    def show_edges(self, node):
+        return node, self.graph[node]
+a = Graph()
+a.add_edge("A")
+print(a.print_graph())
+a.add_edge("B", "A")
+print(a.print_graph())
+a.add_edge("C", "A")
+print(a.print_graph())
